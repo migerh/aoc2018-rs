8ae210bc55c5344040c8f9754d732ee0f9a83583
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2018-12-11 15:54:43 +0100

    Linearize the grid

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,3 +1,9 @@
+fn get_index(cell: (usize, usize)) -> usize {
+  let (x, y) = cell;
+
+  (x - 1) * 300 + y - 1
+}
+
 fn cell_power_level(cell: (usize, usize), serial: i32) -> i32 {
   let (x, y) = cell;
   let rack_id = (x + 10) as i32;
@@ -14,41 +20,41 @@ fn cell_power_level(cell: (usize, usize), serial: i32) -> i32 {
   powerlevel - 5
 }
 
-fn power_level_square(grid: &Vec<Vec<i32>>, top_left: (usize, usize), size: usize) -> i32 {
+fn powerlevel_grid(serial: i32) -> Vec<i32> {
+  let mut result = vec![0; 300*300];
+
+  for x in 1..301 {
+    for y in 1..301 {
+      let i = get_index((x, y));
+      result[i] = cell_power_level((x, y), serial);
+    }
+  }
+
+  result
+}
+
+fn power_level_square(grid: &Vec<i32>, top_left: (usize, usize)) -> i32 {
   let mut powerlevel = 0;
   let (tx, ty) = top_left;
-  for x in 0..size {
-    for y in 0..size {
-      powerlevel += grid[tx + x - 1][ty + y - 1];
+  for x in 0..3 {
+    for y in 0..3 {
+      let i = get_index((tx + x, ty + y));
+      powerlevel += grid[i];
     }
   }
 
   powerlevel
 }
 
-fn powerlevel_grid(serial: i32) -> Vec<Vec<i32>> {
-  let mut result = vec![];
-  for x in 1..301 {
-    let mut line = vec![];
-    for y in 1..301 {
-      line.push(cell_power_level((x, y), serial));
-    }
-    result.push(line);
-  }
-
-  result
-}
-
 pub fn problem1() -> ((usize, usize), i32) {
   let mut max_tl = (1, 1);
   let mut max_pl = 0;
-  let size = 3;
 
   let levels_grid = powerlevel_grid(5177);
 
-  for x in 1..299 {
-    for y in 1..299 {
-      let powerlevel = power_level_square(&levels_grid, (x, y), size);
+  for x in 1..298 {
+    for y in 1..298 {
+      let powerlevel = power_level_square(&levels_grid, (x, y));
       if max_pl < powerlevel {
         max_tl = (x, y);
         max_pl = powerlevel;
@@ -62,23 +68,25 @@ pub fn problem1() -> ((usize, usize), i32) {
   (max_tl, max_pl)
 }
 
-fn get_column(grid: &Vec<Vec<i32>>, top_left: (usize, usize), size: usize) -> i32 {
+fn get_column(grid: &Vec<i32>, top_left: (usize, usize), size: usize) -> i32 {
   let (tx, ty) = top_left;
 
   let mut sum = 0;
   for y in ty..(ty + size) {
-    sum += grid[tx + size - 1][y];
+    let i = get_index((tx + size, y + 1));
+    sum += grid[i];
   }
 
   sum
 }
 
-fn get_row(grid: &Vec<Vec<i32>>, top_left: (usize, usize), size: usize) -> i32 {
+fn get_row(grid: &Vec<i32>, top_left: (usize, usize), size: usize) -> i32 {
   let (tx, ty) = top_left;
 
   let mut sum = 0;
   for x in tx..(tx + size) {
-    sum += grid[x][ty + size - 1];
+    let i = get_index((x + 1, ty + size));
+    sum += grid[i];
   }
 
   sum
@@ -99,9 +107,12 @@ pub fn problem2() -> ((usize, usize), usize, i32) {
       for y in 0..end {
         let col = get_column(&grid, (x, y), size);
         let row = get_row(&grid, (x, y), size);
-        let corner = grid[x + size - 1][y + size - 1];
-        levels_grid[x][y] += col + row - corner;
-        let powerlevel = levels_grid[x][y];
+        let corner_index = get_index((x + size, y + size));
+        let corner = grid[corner_index];
+
+        let cell_index = get_index((x + 1, y + 1));
+        levels_grid[cell_index] += col + row - corner;
+        let powerlevel = levels_grid[cell_index];
 
         if max_pl < powerlevel {
           max_tl = (x + 1, y + 1);
