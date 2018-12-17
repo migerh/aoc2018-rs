334904ae0d06621fd4bcee904e7988032d35004a
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2018-12-17 15:24:43 +0100

    Day 17, flow simulation. Works for example but not for input

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::ops::Range;
 use regex::Regex;
 use std::cmp::{min, max};
@@ -10,6 +9,26 @@ struct Board {
   pub offset: Position,
   pub bbox: (Position, Position),
   pub map: Vec<Vec<char>>,
+}
+
+impl Board {
+  pub fn get(&self, pos: Position) -> char {
+    let p = (pos.0 - self.offset.0, pos.1 - self.offset.1);
+    self.map[p.1 as usize][p.0 as usize]
+  }
+
+  pub fn set(&mut self, pos: Position, c: char) {
+    let p = (pos.0 - self.offset.0, pos.1 - self.offset.1);
+    self.map[p.1 as usize][p.0 as usize] = c;
+  }
+}
+
+static debug: bool = true;
+
+fn log(s: String) {
+  if debug {
+    println!("{}", s);
+  }
 }
 
 fn parse_range(regex: &Regex, line: &str) -> Result<Range<i32>, ParseError> {
@@ -24,7 +43,7 @@ fn parse_range(regex: &Regex, line: &str) -> Result<Range<i32>, ParseError> {
   };
 
   let range = match capture.get(3) {
-    Some(v) => start..v.as_str().parse::<i32>()?,
+    Some(v) => start..v.as_str().parse::<i32>()?+1,
     None => start..start+1
   };
 
@@ -64,7 +83,7 @@ fn bounding_box(positions: &Vec<Position>) -> (Position, Position) {
 }
 
 fn initialize() -> Result<Board, Error> {
-  let input = include_str!("./data/input.txt");
+  let input = include_str!("./data/example.txt");
   let mut positions = vec![];
 
   for line in input.split('\n') {
@@ -72,10 +91,11 @@ fn initialize() -> Result<Board, Error> {
   }
 
   let bbox = bounding_box(&positions);
+  // let offset = ((bbox.0).0 - 1, (bbox.0).1);
   let offset = ((bbox.0).0 - 1, 0);
   let size = ((bbox.1).0 - offset.0 + 1, (bbox.1).1 - offset.1);
 
-  println!("The map goes from {:?} to {:?}, size is {:?}", bbox.0, bbox.1, size);
+  log(format!("The map goes from {:?} to {:?}, size is {:?}", bbox.0, bbox.1, size));
 
   let mut map = vec![vec!['.'; (size.0 + 1) as usize]; (size.1 + 1) as usize];
 
@@ -89,15 +109,158 @@ fn initialize() -> Result<Board, Error> {
 }
 
 fn print(board: &Board) {
-  for line in &board.map {
+  let offset = board.offset;
+  let cols = board.map[0].len() as i32;
+  for l in 0..3 {
+    print!("    ");
+    for x in 0..cols {
+      let n = x + offset.0;
+      let c = (n / (10 as i32).pow(2 - l)) % 10;
+      print!("{}", c);
+    }
+    println!("");
+  }
+
+  for (row, line) in board.map.iter().enumerate() {
     let s: String = line.iter().collect();
+    print!("{:04}", row);
     println!("{}", s);
   }
 }
 
+fn is_sand(c: char) -> bool {
+  c == '.'
+}
+
+fn find_next_stop_down(seed: Position, mut board: &mut Board) -> Position {
+  let (x, mut y) = seed;
+  let max = board.map.len() as i32;
+
+  y += 1;
+  while y < max && is_sand(board.get((x, y))) {
+    board.set((x, y), 'w');
+    y += 1;
+  }
+
+  (x, y)
+}
+
+fn fill_bucket(seed: Position, board: &mut Board) -> Vec<Position> {
+  let (x, y) = seed;
+
+  let mut flow_left = true;
+  let mut flow_right = true;
+  let mut new_seeds = vec![];
+  let width = board.offset.0 + (board.map[0].len() as i32);
+  for dx in 1..width {
+    if !flow_left && !flow_right {
+      break;
+    }
+
+    log(format!("Flowing {} of {}", dx, width));
+
+    if x - dx < 0 {
+      flow_left = false;
+    }
+
+    if x + dx >= width {
+      flow_right = false;
+    }
+
+    log(format!("Flow left, looking at {}, {}", x - dx, y));
+    if flow_left && is_sand(board.get((x - dx, y + 1))) {
+      log(format!("Left: Sand below"));
+      new_seeds.push((x - dx, y));
+      flow_left = false;
+    }
+
+    if flow_left && !is_sand(board.get((x - dx, y))) {
+      log(format!("Left: Hit wall"));
+      flow_left = false;
+    }
+
+    if flow_left {
+      log(format!("Set {}, {}", x - dx, y));
+      board.set((x - dx, y), 'w');
+    }
+
+    log(format!("Flow right, looking at {}, {}", x + dx, y));
+    if flow_right && is_sand(board.get((x + dx, y + 1))) {
+      log(format!("Right: Sand below"));
+      new_seeds.push((x + dx, y));
+      flow_right = false;
+    }
+
+    if flow_right && !is_sand(board.get((x + dx, y))) {
+      log(format!("Right: Hit wall"));
+      flow_right = false;
+    }
+
+    if flow_right {
+      log(format!("Set {}, {}", x + dx, y));
+      board.set((x + dx, y), 'w');
+    }
+  }
+
+  log(format!("New seeds: {:?}", new_seeds));
+  new_seeds
+}
+
+fn trace(seed: Position, mut board: &mut Board) {
+  let mut seeds = vec![seed];
+  let max_y = board.map.len() as i32;
+
+  while !seeds.is_empty() {
+    let next = seeds.pop().unwrap();
+    board.set(next, 'w');
+    let (x, mut y) = find_next_stop_down(next, &mut board);
+
+    if y >= max_y {
+      continue;
+    }
+
+    log(format!("Stop down: {:?}", (x, y)));
+
+    y -= 1;
+    let mut next_seeds = fill_bucket((x, y), &mut board);
+    while next_seeds.is_empty() {
+      y -= 1;
+      log(format!("Fill {}, {}", x, y));
+      next_seeds = fill_bucket((x, y), &mut board);
+    }
+
+    seeds.append(&mut next_seeds);
+  }
+}
+
+fn count_water(board: &Board) -> i32 {
+  let skip_rows = (board.bbox.0).1;
+  // todo: calculate this instead of hard wiring it
+  let skip_cols = 1;
+
+  let mut count = 0;
+  for row in board.map.iter().skip(skip_rows as usize) {
+    for chr in row.iter().skip(skip_cols as usize) {
+      if *chr == 'w' {
+        count += 1;
+      }
+    }
+  }
+
+  count
+}
+
 pub fn problem1() -> Result<(), Error> {
-  let board = initialize()?;
+  let mut board = initialize()?;
   print(&board);
+
+  println!("Tracing water…");
+
+  trace((500, 0), &mut board);
+  print(&board);
+
+  let result = count_water(&board);
+  println!("Result: {}", result);
 
   Ok(())
 }
