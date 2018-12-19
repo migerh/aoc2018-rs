b15c3a6cb288ad946fa9efb853791fe0c306d6cc
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2018-12-19 19:10:51 +0100

    Day 19, Problem 2

--- src/day19/problems.rs
+++ src/day19/problems.rs
@@ -62,8 +62,12 @@ pub fn problem1() -> Result<(), Error> {
     let (op, p) = instruction;
     state = state.apply(op.as_str(), p[0], p[1], p[2])?;
     i += 1;
-    if i % 1_000_000 == 0 {
+    if i % 1_000 == 0 {
       println!("Number of cycles: {}", i);
+      // check the reverse engineered init code
+      // Set the r[0] register to 1 and let it run
+      // for 1000 cycles before cancelling it.
+      println!("r[1] = {}", state.registers[1]);
     }
   }
 
@@ -81,52 +85,57 @@ pub fn problem2() -> Result<(), Error> {
   Ok(())
 }
 
-fn program() -> i128 {
-  let mut r: [i128; 6] = [0, 0, 0, 0, 0, 0];
+fn program() -> i32 {
+  // analyzing the source code tells us that it calculates the sum
+  // of all divisors of 10551398. We can calculate that with Wolfram
+  // Alpha and just sum it up instead of cooking our CPU...
+  // from: https://www.wolframalpha.com/input/?i=divisors+of+10551398
+  let divisors = [1, 2, 11, 13, 22, 26, 79, 143, 158, 286, 467, 869, 934, 1027, 1738, 2054, 5137, 6071, 10274, 11297, 12142, 22594, 36893, 66781, 73786, 133562, 405823, 479609, 811646, 959218, 5275699, 10551398];
 
-  r[1] += 2;
-  r[1] = r[1]*r[1]*19*11 + (r[3] + 7) * 22 + 8;
+  let result = divisors.iter().sum();
+  println!("Result: {}", result);
 
-  if r[0] == 1 {
-    r[4] += 29;
-    r[1] += 27*28*30*14*32;
-    r[0] = 0;
-  }
+  result
 
-  r[4] = 1;
+  // let mut r: [i128; 6] = [1, 0, 0, 0, 0, 0];
 
-  loop {
-    r[2] = 1;
+  // // r[1] += 2;
+  // // r[1] = r[1]*r[1]*19*11 + (r[3] + 7) * 22 + 8;
+  // r[1] = 998;
 
-    loop {
-      r[3] = r[4] * r[2];
+  // if r[0] == 1 {
+  //   // r[4] += 29;
+  //   // r[1] += 27*28*30*14*32;
+  //   // r[1] += 10160640;
 
-      if r[3] == r[1] {
-        r[0] += r[4];
-      }
+  //   // looks like I made a mistake while reverse engineering this program
+  //   // this is the correct value, taken from running the init code in the VM.
+  //   r[1] = 10551398;
+  //   r[0] = 0;
+  // }
 
-      r[2] += 1;
+  // r[4] = 1;
 
-      if r[2] > r[1] {
-        r[4] += 1;
-        break;
-      }
-    }
+  // loop {
+  //   r[2] = 1;
 
-    if r[4] > r[1] {
-      break;
-    }
-  }
+  //   loop {
+  //     r[3] = r[4] * r[2];
 
-  r[0]
-}
+  //     if r[3] == r[1] {
+  //       r[0] += r[4];
+  //     }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
+  //     r[2] += 1;
 
-  #[test]
-  fn check_problem1() {
-    // assert_eq!(problem2().unwrap(), 674);
-  }
+  //     if r[2] > r[1] {
+  //       r[4] += 1;
+  //       break;
+  //     }
+  //   }
+
+  //   if r[4] > r[1] {
+  //     break;
+  //   }
+  // }
 }
